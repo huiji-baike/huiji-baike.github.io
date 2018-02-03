91f3810a665d1a37a6af73e9a09a4fe076150834
chatbot <chatbot@users>
2018-02-03 01:16:25 -0800

    首

--- 广告.js
+++ 广告.js
@@ -519,8 +519,8 @@ document.getElementById("begin-Notification").addEventListener("click", function
 	追踪项 = document.getElementById("tracked-Items").value.split(",");
 	静音时间 = document.getElementById("silent-Interval").value
 	console.log(静音时间)
-	console.log(!静音时间.match(/[0-9]{1,2}/i))
-	if (!静音时间.match(/[0-9]{1,2}/i)){
+	console.log(!静音时间.match(/^[0-9]{1,2}$/i))
+	if (!静音时间.match(/^[0-9]{1,2}$/i)){
 		return 0
 	} else {
 		静音时间 = parseInt(静音时间)

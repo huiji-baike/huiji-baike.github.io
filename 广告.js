89387d43574bb9e549c619f88336c251148ed922
chatbot <chatbot@users>
2018-02-07 00:06:34 -0800

    首

--- 广告.js
+++ 广告.js
@@ -656,7 +656,7 @@ enableInstantSearch && searchInput.addEventListener("input", function (a) {
 
 searchForm.addEventListener("submit", function (a) {
 	a.preventDefault()
-	searchInput.value = searchInput.value.replace(/^名=(.+?)$/gi,'author:"$1"')
+	searchInput.value = searchInput.value.replace(/^名=(.+?)$/gi,"author:\"$1\"")
 	navigate(searchInput.value)
 })
 
